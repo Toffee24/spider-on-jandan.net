9335b9bd4c0b06fc0ea027f582b59848b71eb847
Toffee24 <toffee24@foxmail.com>
2018-06-20 15:34:12 +0800

    Update orm.js
    更改数据库model数据结构

--- orm.js
+++ orm.js
@@ -1,9 +1,9 @@
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('pic_url','root','root',{
-    host:'localhost',
-    port:'3306',
-    dialect:'mysql',
+const sequelize = new Sequelize('pic_url', 'root', 'root', {
+    host: 'localhost',
+    port: '3306',
+    dialect: 'mysql',
     timezone: '+08:00'
 })
 sequelize
@@ -15,11 +15,20 @@ sequelize
         console.error('Unable to connect to the database:', err)
     })
 
-const Url = sequelize.define('url',{
-    pic_url:{
+const Url = sequelize.define('url', {
+    pic_url: {
         unique: true,
-        type:Sequelize.STRING
+        type: Sequelize.STRING,
+    },
+    xx: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null
+    },
+    oo: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null
     }
 })
-
 module.exports = Url
